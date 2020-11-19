261cdc0a8d7560ae102de0227e11e77b7bdf5710
James Yeates <jimibue@gmail.com>
2020-11-19 12:24:25 -0700

    courses functionailty added

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,20 @@
 class CoursesController < ApplicationController
   def index
+    @courses = Course.all
   end
 
   def show
+    @course = Course.find(params[:id])
   end
 
   def new
+    @course = Course.new
+  end
+
+  def create
+    # bad not santizing params but should work
+    # or do error checking
+    @course = Course.create(name: params[:course][:name])
+    redirect_to @course
   end
 end
